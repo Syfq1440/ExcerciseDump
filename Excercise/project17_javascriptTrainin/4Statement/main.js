381fda04636e52123a86dc9af6269470074dd495
Syfq1440 <69136710+Syfq1440@users.noreply.github.com>
2023-05-14 13:24:28 +0800

    New JS train

--- Excercise/project17_javascriptTrainin/4Statement/main.js
+++ Excercise/project17_javascriptTrainin/4Statement/main.js
@@ -1,7 +1,36 @@
 let x, y, z;  // Statement 1
-    x = 5;        // Statement 2
-    y = 6;        // Statement 3
-    z = x + y;    // Statement 4
+x = 5;        // Statement 2
+y = 6;        // Statement 3
+z = x + y;    // Statement 4
     
-    document.getElementById("demo").innerHTML =
-    "The value of z is " + z + ".";  
+document.getElementById("demo").innerHTML =
+"The value of z is " + z + ".";  
+
+// --------------------------------------------------------------------- 
+
+// JavaScript Statements 1.1
+// SEMICOLONS ";"
+// Semicolons separate JavaScript statements.
+// Add a semicolon at the end of each executable statement:
+
+let a, b, c;  // Declare 3 variables
+a = 5;        // Assign the value 5 to a
+b = 6;        // Assign the value 6 to b
+c = a + b;    // Assign the sum of a and b to c
+
+// When separated by semicolons, multiple statements on one line are allowed:
+
+a = 5; b = 6; c = a + b;
+
+// --------------------------------------------------------------------- 
+
+// JavaScript Statements 1.2
+// JavaScript Code Blocks
+// -JavaScript statements can be grouped together in code blocks, inside curly brackets {...}.
+// -The purpose of code blocks is to define statements to be executed together.
+// -One place you will find statements grouped together in blocks, is in JavaScript functions: 
+
+  function myFunction() {
+    document.getElementById("demo1").innerHTML = "Hello Dolly!";
+    document.getElementById("demo2").innerHTML = "How are you?";
+  }
---
+++ Excercise/project17_javascriptTrainin/5Syntax/main.js
@@ -0,0 +1,49 @@
+
+// JavaScript syntax is the set of rules, how JavaScript programs are constructed:
+
+// How to create variables:
+// var x;
+// let y;
+
+// How to use variables:
+// x = 5;
+// y = 6;
+// let z = x + y;
+
+// --------------------------------------------------------------------- 
+
+// ------ JavaScript Values ---------
+// The JavaScript syntax defines two types of values:
+
+//     - Fixed values == Literals
+//     - Variable values == Variables
+
+// Fixed values are called Literals.
+// Variable values are called Variables.
+
+// --- JavaScript Literals ---
+// The two most important syntax rules for fixed values are:
+// 1. Numbers are written with or without decimals:
+    // - 10.50
+    // - 1001
+// 2. Strings are text, written within double or single quotes:
+    // - "John Doe"
+    // - 'John Doe'
+
+// --- JavaScript Variables ---
+// In a programming language, variables are used to store data values.
+// JavaScript uses the keywords "var, let and const" to declare variables.
+// An equal sign is used to assign values to variables.
+// In this example, x is defined as a variable. Then, x is assigned (given) the value 6:
+    // let x;
+    // x = 6; 
+
+    // -- JavaScript Operators --
+    // JavaScript uses arithmetic operators ( + - * / ) to compute values: 
+        // (5 + 6) * 10
+    // JavaScript uses an assignment operator ( = ) to assign values to variables: 
+        // let x, y;
+        // x = 5;
+        // y = 6; 
+    
+// --------------------------------------------------------------------- 
